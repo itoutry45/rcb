2b5be0573e31b601c4bd603178111cb913429ad8
itoutry45 <115962505+itoutry45@users.noreply.github.com>
2022-11-27 16:58:23 +0600

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -13,8 +13,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 # variables
 API_ID = config("API_ID", "19802104")
-API_HASH = config("API_HASH", "
-")
+API_HASH = config("API_HASH", "6be78ddee47c08e9ef348d5b2fc6bd84")
 BOT_TOKEN = config("BOT_TOKEN", "5689294860:AAHxa7B3ULv8wjcUJpBTr6YiR_3CLtpvsTk")
 SESSION = config("SESSION", "BQDE8YBhIhzYNKfO3XlH2NgPew5GPp6vNbF0UtuHVB79qqe8vrdLSOR-6tEXGrDIqtIOEDLp2JteQS07EpmdvfZma6FYvp32SINTlgKLVd8zh4iLA4T0ZnrMI8G18SUx36j5kUqnMNQBtUh-8JRr6dhyhTABkj_Y4L7q79v82u7Rn1u3GYh29kc2s9PdRIPG2uRlByGGd6m84lTw9CRkXUE4CFLMHgVFWpaDBrOQE1wmZoB21vvrFCGLf-ZUwyPpevJrUX-cPqryy0QBuaWki78IUSNsSCP_3A67ZzppmXVd5hd0T2PfjkgfjxHHX5ete0wROuErIgHXtEjDipz9ARIPAAAAAT-RHTgA")
 FORCESUB = config("FORCESUB", "rcb2_b")
