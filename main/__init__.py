ef25d07116f4d40d051d6039d4d9d9cf29425a75
itoutry45 <115962505+itoutry45@users.noreply.github.com>
2022-11-30 21:52:54 +0600

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", "19802104")
-API_HASH = config("API_HASH", "6be78ddee47c08e9ef348d5b2fc6bd84")
-BOT_TOKEN = config("BOT_TOKEN", "5689294860:AAHxa7B3ULv8wjcUJpBTr6YiR_3CLtpvsTk")
-SESSION = config("SESSION", "BQDE8YBhIhzYNKfO3XlH2NgPew5GPp6vNbF0UtuHVB79qqe8vrdLSOR-6tEXGrDIqtIOEDLp2JteQS07EpmdvfZma6FYvp32SINTlgKLVd8zh4iLA4T0ZnrMI8G18SUx36j5kUqnMNQBtUh-8JRr6dhyhTABkj_Y4L7q79v82u7Rn1u3GYh29kc2s9PdRIPG2uRlByGGd6m84lTw9CRkXUE4CFLMHgVFWpaDBrOQE1wmZoB21vvrFCGLf-ZUwyPpevJrUX-cPqryy0QBuaWki78IUSNsSCP_3A67ZzppmXVd5hd0T2PfjkgfjxHHX5ete0wROuErIgHXtEjDipz9ARIPAAAAAT-RHTgA")
-FORCESUB = config("FORCESUB", "rcb2_b")
-AUTH = config("AUTH", "5361442104")
+API_ID = 19802104
+API_HASH = "6be78ddee47c08e9ef348d5b2fc6bd84"
+BOT_TOKEN = "5689294860:AAHxa7B3ULv8wjcUJpBTr6YiR_3CLtpvsTk"
+SESSION = "BQB6JIa99tQvo7GItKUKq0_wm2a6Ul7T1p_Fogv7lNImSfj69x_p0ROWgMROsnAoGCEATGAIlPFtF07_ptUpHMDbVERtUxNpsU6QVn6eKCQq4e0v0mH65sGmPoldYBPM6MN4ROPtBjqSiqe4uGpDxAIU_W5s1ODKVDfoZ5l_pARcUfQDjvwjc1UyKZenBJrY9R_zhzygt_HaTnQ9iuauFt2cc0P1pbVu5MBGnz3sXoscxj96b7nTG1kqSeqw_wq8YWjTAceNmBUbWxMiE3MncHjdJuZ3szRenN3F-eUmpVbeUPz16QfJMixh0k8ISrXQ5MfaJ7RezkoJ4vladYgO47aJAAAAAT-RHTgA"
+FORCESUB = "rcb2_b"
+AUTH = 5361442104
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
